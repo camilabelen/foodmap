3d69b92a3fc1cedacd9f05fc6d3fa797c3b5dfae
camilabelen <ccornejoss@gmail.com>
2017-12-14 10:29:23 -0300

    cambios en html

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,24 +55,5 @@ $(document).ready(function(){
   
 });
 
-/*
-$('.category_list .category_item[category="all"]').addClass('ct_item-active');
 
-$('.category_item').click(function(){
-
-
-  var catProduct = $(this).attr('category');
-  $('.category_item').removeClass('ct_item-active');
-  $(this).addClas('ct_item-active');
-
-  //ocultando productos
-  $('.product-item').hide();
-  //mostrando 
-  $('.product-item[category="'+catProducts+'"]').show()
-})
-
-$('.category_item[category="all"]').click(function(){
-  $('.product-item').show();
-})
-*/
 
